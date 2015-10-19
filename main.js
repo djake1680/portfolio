68a318876109f35f21fcaae40e21a50c787d76f9
Darin Jacobson <djake1680@gmail.com>
2015-10-19 13:29:00 -0700

    portfolio

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.section_link').click(function (event) {
+        event.stopPropagation();
+        var idTo = $(this).attr('data-atr');
+        var Position = $('[id="' + idTo + '"]').offset().top;
+        jQuery('html, body').animate({ scrollTop: Position }, 'slow');
+        return false;
+    });
+
+    $(".top_link").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false; // prevents the default action of the event being triggered
+    });
+
+}); // end of document ready
